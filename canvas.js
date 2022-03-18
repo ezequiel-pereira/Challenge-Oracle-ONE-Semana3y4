e823d7c85d4923ebf5d272dd4e2c5e51ae360802
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-03-18 19:50:56 -0300

    Función para dibujar letras y guiones

--- canvas.js
+++ canvas.js
@@ -10,11 +10,16 @@ function crearCanvas() {
   ctx = tablero.getContext('2d');
 }
 
-function dibujarGuiones(letras, x, y) {
-  for (let index = 0; index < letras; index++) {
-    ctx.moveTo(x, y);
-    ctx.lineTo(x + 30, y);
-    x += 35;
-    ctx.stroke();
-  }
+function dibujarGuiones(length, x, y) {
+  let guiones = Array(length);
+  guiones.fill('_');
+  dibujarLetras(guiones, x, y);
+}
+
+function dibujarLetras(letras, x, y) {
+  ctx.fillStyle = 'white';
+  ctx.fillRect(x, (y-50), 600, 60);
+  ctx.fillStyle = 'black';
+  ctx.font = '48px serif';
+  ctx.fillText(letras.join(' '), x, y);
 }
