accbf3d286d25f0a8a6e8bca816fca07b0a4215c
Ezequiel Pereira <ezequielpereira92@gmail.com>
2022-03-21 20:29:13 -0300

    Verificar si la letra ingresada es correcta

--- ahorcado.js
+++ ahorcado.js
@@ -1,35 +1,83 @@
-var botonIniciar = document.getElementById('iniciar-juego');
-var palabras = ['COMPUTADORA', 'BOTELLA', 'CELULAR', 'ALCOHOL', 'DESTORNILLADOR'];
-var palabra = '';
-var letrasCorrectas= [];
+var botonIniciar = document.getElementById("iniciar-juego");
+var palabras = [
+  "COMPUTADORA",
+  "BOTELLA",
+  "CELULAR",
+  "ALCOHOL",
+  "DESTORNILLADOR",
+];
+var palabra = "";
+var errores = 0;
+var letrasCorrectas = [];
+var letrasIncorrectas = [];
 
 function iniciarJuego(event) {
   event.preventDefault();
-  document.getElementById('iniciar-juego-div').remove();
-  document.getElementById('agregar-palabra-div').remove();
+  document.getElementById("iniciar-juego-div").remove();
+  document.getElementById("agregar-palabra-div").remove();
   crearCanvas();
   palabra = elegirPalabra(palabras);
-  palabra = palabra.split('');
-  letrasCorrectas = Array(palabra.length).fill('_');
-  dibujarLetras(letrasCorrectas, 300, 450);
-  document.onkeypress = validarLetraIngresada;
+  palabra = palabra.split("");
+  letrasCorrectas = Array(palabra.length).fill("_");
+  dibujarLetras(letrasCorrectas, 800, 500, true);
+  dibujarHorca();
+  document.onkeypress = juego;
 }
 
-botonIniciar.addEventListener('click', iniciarJuego);
+botonIniciar.addEventListener("click", iniciarJuego);
+
+function juego(event) {
+  let letra = event.key;
+
+  if (errores < 6) {
+    console.log("Errores: " + errores);
+  } else {
+    console.log("Errores: " + errores);
+    return false;
+  }
+
+  if (validarLetraIngresada(letra)) {
+    console.log("Letra válida");
+  } else {
+    return false;
+  }
+
+  if (letraCorrecta(letra)) {
+    dibujarLetras(letrasCorrectas, 600, 450, true);
+  } else {
+    letrasIncorrectas.push(letra);
+    dibujarLetras(letrasIncorrectas, 800, 200, false);
+    errores++;
+    dibujarAhorcado(errores);
+  }
+}
 
 function elegirPalabra(palabras) {
-  let indice = Math.floor((Math.random() * palabras.length));
+  let indice = Math.floor(Math.random() * palabras.length);
   return palabras[indice];
 }
 
 function validarLetraIngresada(event) {
   let letra = event;
-  let esUnaLetraMayuscula  = new RegExp('[A-Z]');
+  let esUnaLetraMayuscula = new RegExp("[A-Z]");
   if (letra.length === 1 && esUnaLetraMayuscula.test(letra)) {
-    console.log(letra + ' es una letra mayúscula');
+    console.log(letra + " es una letra mayúscula");
     return true;
   } else {
-    console.log(letra + ' no es una letra mayúscula');
+    console.log(letra + " no es una letra mayúscula");
     return false;
+  }
+}
+
+function letraCorrecta(letra) {
+  let aciertos = 0;
+  palabra.forEach((element, index) => {
+    if (element === letra) {
+      letrasCorrectas[index] = element;
+      aciertos++;
+    }
+  });
+  if (aciertos >= 1) {
+    return true;
   }
 }
